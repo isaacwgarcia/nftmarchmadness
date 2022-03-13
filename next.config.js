132d7a8fe73f18b4690e10fd861f58941797faef
Isaac Garcia <isaacwi@gmail.com>
2022-03-12 21:06:42 -0500

    initial load

--- next.config.js
+++ next.config.js
@@ -10,6 +10,9 @@ module.exports = withLess({
   env: {
     INFURA_IPFS: process.env.INFURA_IPFS,
     TEAMSA_IPFS: process.env.TEAMSA_IPFS,
+    TEAMSB_IPFS: process.env.TEAMSB_IPFS,
+    TEAMSC_IPFS: process.env.TEAMSC_IPFS,
+    TEAMSD_IPFS: process.env.TEAMSD_IPFS,
     TABLELAND_ENDPOINT: process.env.TABLELAND_ENDPOINT,
     TABLELAND_ENDPOINT_RPC: process.env.TABLELAND_ENDPOINT_RPC,
   },
